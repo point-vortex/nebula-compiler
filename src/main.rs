3be5eecbcf3b1278d75d06f553265293cf0eb7f0
DanilAndreev <danssg08@gmail.com>
2021-12-12 21:55:52 +0200

    [GLOBAL] Added simple code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
+mod tokenizer;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
---
+++ src/tokenizer.rs
@@ -0,0 +1,5 @@
+struct Tokens {
+    input: String,
+    cursor: usize,
+}
+
